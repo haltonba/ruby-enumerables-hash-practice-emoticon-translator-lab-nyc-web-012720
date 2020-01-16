444c8fabdee03b0df61b13674d16e7967da65f49
Halton Bagley <haltonba94@gmail.com>
2020-01-16 21:34:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,4 +24,9 @@ end
 
 def get_english_meaning (file_path = "./lib/emoticon.yml", japanese_emoticon)
   library = load_library(file_path)
+  if library[:get_meaning].include? japanese_emoticon
+    library[:get_meaning][japanese_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
