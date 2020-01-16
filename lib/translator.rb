3cfec1e3c4b7f0353cebd95afe237a9fa205c237
Halton Bagley <haltonba94@gmail.com>
2020-01-16 20:29:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,8 @@ require 'yaml'
 def load_library (file_path)
   emoticons = YAML.load_file(file_path)
   hash = {
-    get_meaning: {},
-    get_emoticon: {}
+    "get_meaning": {},
+    "get_emoticon": {}
   }
   emoticons
 end
